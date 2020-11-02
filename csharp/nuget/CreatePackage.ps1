e4cd150b0897db52fb811920518f8845a22bcf10
Tobias Gaertner <tgaertner@recom.eu>
2020-11-02 16:08:29 +0100

    specialize nuget package content
    - copy/clean native file during build

--- csharp/nuget/CreatePackage.ps1
+++ csharp/nuget/CreatePackage.ps1
@@ -5,7 +5,8 @@ if (!(Test-Path $nugetCLI)) {
     Invoke-WebRequest -Uri https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile $nugetCLI
 }
 
-& $msbuild ..\Windows\Facebook.Yoga.Desktop.sln /m:4 /nodeReuse:false /p:Configuration=Release /p:Platform="x64" /t:Build
-& $msbuild ..\Windows\Facebook.Yoga.Desktop.sln /m:4 /nodeReuse:false /p:Configuration=Release /p:Platform="x86" /t:Build
+& $msbuild ..\Windows\Facebook.Yoga\Facebook.Yoga.csproj /m:4 /nodeReuse:false /p:Configuration=Release /p:Platform="AnyCPU" /t:Rebuild
+& $msbuild ..\Windows\Facebook.Yoga.Desktop.sln /m:4 /nodeReuse:false /p:Configuration=Release /p:Platform="x64" /t:Rebuild
+& $msbuild ..\Windows\Facebook.Yoga.Desktop.sln /m:4 /nodeReuse:false /p:Configuration=Release /p:Platform="x86" /t:Rebuild
 
 .\nuget.exe pack .\Facebook.Yoga.nuspec
